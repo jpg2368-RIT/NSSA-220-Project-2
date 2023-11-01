fe42dc41102bb072b7c0b938e9690f64c531ac63
School <jpg2368@rit.edu>
2023-11-01 15:14:10 -0400

    renamed variables and reformated

--- filter_packets.py
+++ filter_packets.py
@@ -1,26 +1,26 @@
 # sudo pip3 install scapy
 from scapy.all import *
 
-def filter(packets, layer) :
-	filteredPackets=[]
-	for packet in packets:
-		if (packet.haslayer(layer)):
-			filteredPackets.append(packet)
-	return filteredPackets
+
+def filter(packets, layer):
+    filtered_packets = []
+    for packet in packets:
+        if packet.haslayer(layer):
+            filtered_packets.append(packet)
+    return filtered_packets
 
 
-def main() :
-	filePath = "./Captures/"
-	filename = "Node1.pcap"
-	packets = rdpcap(filePath + filename)
-	layer="ICMP"
+def main():
+    file_path = "./Captures/"
+    filename = "Node1.pcap"
+    packets = rdpcap(file_path + filename)
+    layer = "ICMP"
 
-	filteredPackets = filter(packets, layer)
+    filtered_packets = filter(packets, layer)
 
-	for packet in filteredPackets:
-		print(packet)
-	
+    for packet in filtered_packets:
+        print(packet)
 
-if __name__=="__main__":
-	main()
-	
+
+if __name__ == "__main__":
+    main()
