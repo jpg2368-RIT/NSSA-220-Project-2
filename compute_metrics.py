c4329f05d29403da44d851cb97b82c9b17f77b22
School <jpg2368@rit.edu>
2023-11-01 15:35:17 -0400

    added comment block-out

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,3 +1,24 @@
-def compute() :
-	print('called compute function in compute_metrics.py')
+def compute(metrics):
+    for metric in metrics:
+        # Data size metrics
+        # ==========================
+        # num echo requests sent
+        # num echo requests received
+        # num echo replies sent
+        # num echo replies received
+        # Total request bytes sent
+        # total request bytes received
+        # total request data sent
+        # total request data received
 
+        # Time-based metrics
+        # ============================
+        # average round trip time in ms
+        # throughput
+        # goodput
+        # average reply delay in microseconds
+
+        # Distance metric
+        # ============================
+        # average number of hops per echo request
+        pass
