0bec3d10f371259619af91076539bb49d82a6704
School <jpg2368@rit.edu>
2023-11-01 15:24:05 -0400

    blocked out the main run file

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,6 +2,14 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-filter()
-parse()
-compute()
+
+def main():
+    num_nodes = 4
+    for i in range(num_nodes):
+        filtered_packets = filter(f"./Captures/Node{i}.pcap", "ICMP")
+        parsed_metrics = parse(filtered_packets)
+        computed_metrics = compute(parsed_metrics)
+
+
+if __name__ == "__main__":
+    main()
