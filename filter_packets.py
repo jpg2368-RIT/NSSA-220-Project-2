b7894d30dfcec1bbef7d5ea03e1ccaf0594407a3
Vincent Son <ves7357@g.rit.edu>
2023-11-03 02:29:55 -0400

    filter_packets version 2 generator

--- filter_packets.py
+++ filter_packets.py
@@ -7,16 +7,13 @@ def filter(file, layer) :
 	if not file_exist(file):
 		print(file, "does not exist!")
 		return False
-	
-	with open(directory + filename + "_filtered.txt", "w") as output_file:
-		index = 0
-		while True:
-			packet, index = extract_packet(file, index)
-			if packet == "":
-				break
-			if re.search(layer, packet):
-				print(packet)
-				output_file.write(packet)			
+	with open(file) as input_file:
+		packet_generator = extract_packet(input_file)
+		with open(directory + filename + "_filtered.txt", "w") as output_file:
+			for packet in packet_generator:
+				if re.search(layer, packet):
+					# print(packet)
+					output_file.write(packet)			
 
 def file_exist(file):
     try:
@@ -48,24 +45,21 @@ def split_file(file):
 	return directory, filename, extension
 
 # extracts packet starting from header
-def extract_packet(file, index = 0):
+def extract_packet(file):
 	start_extract = False
 	packet = ""
-	with open(file) as file:
-		for _ in range(index):
-			next(file)
-		for line in file:
-			header_match = bool(re.search("^No\.", line))
-			# start extracting if header match and is not extracting. Stop when header match and is extracting
-			if not start_extract and header_match:
-				start_extract=True
-			elif start_extract and header_match:
-				return packet, index
-			if start_extract:
-				packet += line
-				index += 1
+	for line in file:
+		header_match = bool(re.search("^No\.", line))
+		# start extracting if header match and is not extracting. Stop when header match and is extracting
+		if not start_extract and header_match:
+			start_extract=True
+		elif start_extract and header_match:
+			yield packet
+			packet = ""
+		if start_extract:
+			packet += line
 
-	return packet, index
+	yield packet
 
 def main() :
 	directory = "./Captures/"
