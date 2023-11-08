284c3fd3c1c38372a0dbc302d9f35d86a0a99974
School <jpg2368@rit.edu>
2023-11-08 16:14:51 -0500

    Reformatted code

--- filter_packets.py
+++ filter_packets.py
@@ -1,20 +1,22 @@
 import re
 
-# check if file exist and is pcap, then create filtered txt
-def filter(file, layer) :
-	directory, filename, extension = split_file(file)
-	filtered_file = directory + filename + "_filtered.txt"
 
-	if not file_exist(file):
-		print(file, "does not exist!")
-		return False
-	with open(file) as input_file:
-		packet_generator = extract_packet(input_file)
-		with open(filtered_file, "w") as output_file:
-			for packet in packet_generator:
-				if re.search(layer, packet):
-					output_file.write(packet)
-	return filtered_file			
+# check if file exist and is pcap, then create filtered txt
+def filter(file, layer):
+    directory, filename, extension = split_file(file)
+    filtered_file = directory + filename + "_filtered.txt"
+
+    if not file_exist(file):
+        print(file, "does not exist!")
+        return False
+    with open(file) as input_file:
+        packet_generator = extract_packet(input_file)
+        with open(filtered_file, "w") as output_file:
+            for packet in packet_generator:
+                if re.search(layer, packet):
+                    output_file.write(packet)
+    return filtered_file
+
 
 def file_exist(file):
     try:
@@ -23,54 +25,56 @@ def file_exist(file):
     except FileNotFoundError:
         return False
 
+
 # split input into directory, filename, and extension
 def split_file(file):
-	"""
+    """
 		For testing purpose
 		# print("last slash dot index:", last_slash, last_dot)
 		# print(file)
 		# print ("directory", directory,"\nfilename:", filename, "\nextension:", extension)
 	"""
 
-	last_slash = file.rfind("/") + 1
-	# end at specific index
-	directory = file[:last_slash]
-	filename_extension = file[last_slash:]
+    last_slash = file.rfind("/") + 1
+    # end at specific index
+    directory = file[:last_slash]
+    filename_extension = file[last_slash:]
 
-	last_dot = filename_extension.rfind(".")
-	if (last_dot == -1):
-		last_dot = len(filename_extension)
-	filename = filename_extension[:last_dot]
-	extension = filename_extension[last_dot:]
+    last_dot = filename_extension.rfind(".")
+    if last_dot == -1:
+        last_dot = len(filename_extension)
+    filename = filename_extension[:last_dot]
+    extension = filename_extension[last_dot:]
 
-	return directory, filename, extension
+    return directory, filename, extension
+
 
 # extracts packet starting from header
 def extract_packet(file):
-	start_extract = False
-	packet = ""
-	for line in file:
-		header_match = bool(re.search("^No\.", line))
-		# start extracting if header match and is not extracting. Stop when header match and is extracting
-		if not start_extract and header_match:
-			start_extract=True
-		elif start_extract and header_match:
-			yield packet
-			packet = ""
-		if start_extract:
-			packet += line
+    start_extract = False
+    packet = ""
+    for line in file:
+        header_match = bool(re.search("^No\.", line))
+        # start extracting if header match and is not extracting. Stop when header match and is extracting
+        if not start_extract and header_match:
+            start_extract = True
+        elif start_extract and header_match:
+            yield packet
+            packet = ""
+        if start_extract:
+            packet += line
 
-	yield packet
-
-def main() :
-	directory = "./Captures/"
-	file = "Node1.txt"
-	layer="ICMP"
-
-	filter(directory + file, layer)
+    yield packet
 
 
-if __name__=="__main__":
-	main()
-	# split_file("./testing/Project 2/version_96/.The.dothere.txt")
-	
+def main():
+    directory = "./Captures/"
+    file = "Node1.txt"
+    layer = "ICMP"
+
+    filter(directory + file, layer)
+
+
+if __name__ == "__main__":
+    main()
+# split_file("./testing/Project 2/version_96/.The.dothere.txt")
--- packet_parser.py
+++ packet_parser.py
@@ -1,53 +1,57 @@
 from filter_packets import extract_packet
 
-def parse(file) :
-	packets = []
-	with open(file) as file:
-		packet_generator = extract_packet(file)
-		for packet in packet_generator:
-			packets.append(packet_dictionary(packet))
-	return packets
+
+def parse(file):
+    packets = []
+    with open(file) as file:
+        packet_generator = extract_packet(file)
+        for packet in packet_generator:
+            packets.append(packet_dictionary(packet))
+    return packets
+
 
 def packet_dictionary(packet):
-	"""
+    """
 	For testing purpose:
 
 	"""
-	lines = packet.split("\n")
-	cleaned_lines = []
-	for line in lines:
-		cleaned_lines.append(line.split())
-	
-	# ['No.', 'Time', 'Source', 'Destination', 'Protocol', 'Length', 'Info', 'Hex']
-	header = None
-	packet_dict = {"Hex" : ""}
-	for i in range(len(cleaned_lines)):
-		if i < 2:
-			if i == 0:
-				header = cleaned_lines[i]
-				continue
-			if i == 1:
-				for j in range(len(cleaned_lines[i])):
-					# The rest of array will output to Info since Info contain whitespaces
-					if j < len(header):
-						packet_dict[header[j]] = cleaned_lines[i][j]
-					else:
-						packet_dict[header[len(header) - 1]] += cleaned_lines[i][j] + " "
-		else:
-			for j in range(1, len(cleaned_lines[i]) - 1):
-				packet_dict["Hex"] += cleaned_lines[i][j] + " "
-	
-	return packet_dict
+    lines = packet.split("\n")
+    cleaned_lines = []
+    for line in lines:
+        cleaned_lines.append(line.split())
+
+    # ['No.', 'Time', 'Source', 'Destination', 'Protocol', 'Length', 'Info', 'Hex']
+    header = None
+    packet_dict = {"Hex": ""}
+    for i in range(len(cleaned_lines)):
+        if i < 2:
+            if i == 0:
+                header = cleaned_lines[i]
+                continue
+            if i == 1:
+                for j in range(len(cleaned_lines[i])):
+                    # The rest of array will output to Info since Info contain whitespaces
+                    if j < len(header):
+                        packet_dict[header[j]] = cleaned_lines[i][j]
+                    else:
+                        packet_dict[header[len(header) - 1]] += cleaned_lines[i][j] + " "
+        else:
+            for j in range(1, len(cleaned_lines[i]) - 1):
+                packet_dict["Hex"] += cleaned_lines[i][j] + " "
+
+    return packet_dict
+
 
 def main():
-	packets = parse("./Captures/example_filtered.txt")
+    packets = parse("./Captures/example_filtered.txt")
 
-	# for packet in packets:
-	# 	print()
-	# 	print(packet["Hex"])
 
-	# print(packets[0]["Hex"])
-		
+# for packet in packets:
+# 	print()
+# 	print(packet["Hex"])
+
+# print(packets[0]["Hex"])
+
 
 if __name__ == "__main__":
-	main()
+    main()
