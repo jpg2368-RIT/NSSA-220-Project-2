08e70345f5a38095f74cf7effb5794eff3369bd1
Vincent Son <ves7357@g.rit.edu>
2023-11-29 17:33:07 -0500

    minor fix on Info

--- packet_parser.py
+++ packet_parser.py
@@ -17,7 +17,7 @@ def packet_dictionary(packet):
 
     # {'No.', 'Time', 'Source', 'Destination', 'Protocol', 'Length', 'Info', 'Hex'}
     header = None
-    packet_dict = {"Hex": ""}
+    packet_dict = {}
     for i in range(len(cleaned_lines)):
         if i < 2:
             if i == 0:
@@ -25,19 +25,26 @@ def packet_dictionary(packet):
                 continue
             if i == 1:
                 for j in range(len(cleaned_lines[i])):
-                    # The rest of array will output to Info since Info contain whitespaces
+                    # The rest of array will output to Info since Info contain whitespaces. 
+                    # - 1 will end early in order to place whitespace initially 
                     if j < len(header):
                         packet_dict[header[j]] = cleaned_lines[i][j]
                     else:
-                        packet_dict[header[len(header) - 1]] += cleaned_lines[i][j] + " "
+                        packet_dict[header[len(header) - 1]] += " " + cleaned_lines[i][j]
         else:
+            if "Hex" not in packet_dict:
+                packet_dict["Hex"] = ""
             for j in range(1, len(cleaned_lines[i]) - 1):
-                packet_dict["Hex"] += cleaned_lines[i][j] + " "
+                packet_dict["Hex"] += cleaned_lines[i][j] + " "  
 
     return packet_dict
 
 def main():
     packets = parse("./Captures/example_filtered.txt")
 
+    for packet in packets:
+        for key, value in packet.items():
+            print(key + ":", value)
+        
 if __name__ == "__main__":
     main()
