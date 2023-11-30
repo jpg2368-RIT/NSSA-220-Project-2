09685132b2c4bd1a353366a156f2db1ff5c8c443
School <jpg2368@rit.edu>
2023-11-30 13:02:40 -0500

    done except for rounding and fix of how count

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,7 +1,7 @@
 import statistics
 
 
-def between(string, char1, char2):
+def between(string: str, char1: str, char2: str) -> str:
     bet = ""
     reading = False
     for c in string:
@@ -15,20 +15,20 @@ def between(string, char1, char2):
     return bet
 
 
-def find_packet_from_seq(packets, seq):
+def find_packet_from_seq(packets: list, seq: int):
     for packet in packets:
         split_info = packet["Info"].split(" ")
-        if int(between(split_info[4], "=", "/")) == int(seq):
+        if int(between(split_info[4], "=", "/")) == seq:
             return packet
 
 
-def find_no(packets, no):
+def find_no(packets: list, no: int):
     for packet in packets:
-        if packet["No."] == no:
+        if int(packet["No."]) == no:
             return packet
 
 
-def compute(packets, ip):
+def compute(packets: list, ip: str):
     num_echo_requests_sent = 0
     num_echo_requests_received = 0
     num_echo_replies_sent = 0
@@ -45,14 +45,14 @@ def compute(packets, ip):
     open_request_packets = []
     rec_request_packets = []
 
-    # input: ['No.', 'Time', 'Source', 'Destination', 'Protocol', 'Length', 'Info', 'Hex']
+    # inputs: ['No.', 'Time', 'Source', 'Destination', 'Protocol', 'Length', 'Info', 'Hex']
     HEADER_SIZE = 42
     for packet in packets:
         split_info = packet["Info"].split(" ")
         # Data size metrics
         # ==========================
         # num echo requests sent, request bytes sent, request data sent
-        if split_info[2] == "request" and packet["Source"] == str(ip):
+        if split_info[2] == "request" and packet["Source"] == ip:
             num_echo_requests_sent += 1
             # Total request bytes sent
             request_bytes_sent.append(int(packet["Length"]))
@@ -60,7 +60,7 @@ def compute(packets, ip):
             open_request_packets.append(packet)
 
         # num echo requests received, request bytes received, request data received
-        if split_info[2] == "request" and packet["Destination"] == str(ip):
+        if split_info[2] == "request" and packet["Destination"] == ip:
             num_echo_requests_received += 1
             # total request bytes received
             request_bytes_received.append(int(packet["Length"]))
@@ -68,15 +68,15 @@ def compute(packets, ip):
             rec_request_packets.append(packet)
 
         # num echo replies sent
-        if split_info[2] == "reply" and packet["Source"] == str(ip):
+        if split_info[2] == "reply" and packet["Source"] == ip:
             num_echo_replies_sent += 1
-            other = find_packet_from_seq(rec_request_packets, split_info[8][:-1])
+            other = find_no(rec_request_packets, int(split_info[8][:-1]))
             delays.append(float(other["Time"]) - float(packet["Time"]))
 
         # num echo replies received
-        if split_info[2] == "reply" and packet["Destination"] == str(ip):
+        if split_info[2] == "reply" and packet["Destination"] == ip:
             num_echo_replies_received += 1
-            other = find_no(open_request_packets, split_info[8][:-1])
+            other = find_no(open_request_packets, int(split_info[8][:-1]))
             round_trips.append(float(other["Time"]) - float(packet["Time"]))
 
         # Distance metric
@@ -89,32 +89,20 @@ def compute(packets, ip):
 
         # Time-based metrics
         # ============================
-        # TODO: average round trip time in ms (computed at end)
+        # average round trip time in ms (computed at end)
         # time between echo request and corresponding reply (ms)
 
     # throughput
-    try:
-        thruput = sum(request_bytes_sent) / sum(round_trips)
-    except:
-        thruput = "placeholder"
+    thruput = sum(request_bytes_sent) / sum(round_trips)
 
     # goodput
-    try:
-        goodput = sum(request_data_sent) / sum(round_trips)
-    except:
-        goodput = "placeholder"
+    goodput = sum(request_data_sent) / sum(round_trips)
 
     # temp
 
-    # TODO: average reply delay in microseconds
+    # average reply delay in microseconds
     # time between receiving request and sending corresponding reply
-    try:
-        return (num_echo_requests_sent, num_echo_requests_received, num_echo_replies_sent, num_echo_replies_received,
-                sum(request_bytes_sent), sum(request_bytes_received), sum(request_data_sent),
-                sum(request_data_received),
-                statistics.mean(round_trips), thruput, goodput, statistics.mean(delays), statistics.mean(hops))
-    except:
-        return (num_echo_requests_sent, num_echo_requests_received, num_echo_replies_sent, num_echo_replies_received,
-                sum(request_bytes_sent), sum(request_bytes_received), sum(request_data_sent),
-                sum(request_data_received),
-                "placeholder", thruput, goodput, "placeholder", "placeholder")
+    return (num_echo_requests_sent, num_echo_requests_received, num_echo_replies_sent, num_echo_replies_received,
+            sum(request_bytes_sent), sum(request_bytes_received), sum(request_data_sent),
+            sum(request_data_received), statistics.mean(round_trips)*-1000, thruput/-1000, goodput/-1000, statistics.mean(delays)*-1000000,
+            statistics.mean(hops))
