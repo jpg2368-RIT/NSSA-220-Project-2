e25840134a2f0296e30e772bf6a4e2e49d376b11
Vincent Son <ves7357@g.rit.edu>
2023-11-30 19:10:33 -0500

    small change

--- compute_metrics.py
+++ compute_metrics.py
@@ -94,8 +94,9 @@ def compute(packets, ip):
     # average reply delay in microseconds
     # time between receiving request and sending corresponding reply
 
-    return (num_echo_requests_sent, num_echo_requests_received, num_echo_replies_sent, num_echo_replies_received,
-            sum(request_bytes_sent), sum(request_bytes_received), sum(request_data_sent),
-            sum(request_data_received), statistics.mean(round_trips) * -1000, thruput / -1000, goodput / -1000,
-            statistics.mean(delays) * -1000000,
-            statistics.mean(hops))
+    return (
+        num_echo_requests_sent, num_echo_requests_received, num_echo_replies_sent, num_echo_replies_received,
+        sum(request_bytes_sent), sum(request_bytes_received), sum(request_data_sent),
+        sum(request_data_received), statistics.mean(round_trips) * -1000, thruput / -1000, goodput / -1000,
+        statistics.mean(delays) * -1000000,
+        statistics.mean(hops))
